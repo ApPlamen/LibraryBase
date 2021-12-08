719a2280f77c3c7f2b79a9f788f9d222bf2dcf26
Plamen Dobrev <46189922+ApPlamen@users.noreply.github.com>
2021-12-08 14:51:49 +0200

    Add Book and User services

---
+++ src/main/java/com/tusofia/LibraryBase/services/BookService.java
@@ -0,0 +1,10 @@
+package com.tusofia.LibraryBase.services;
+
+import com.tusofia.LibraryBase.entities.Book;
+
+public class BookService extends CRUDSrevice<Book, Integer> {
+
+	public BookService() {
+	}
+
+}
