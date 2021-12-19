9d349f298f9f348de5fa6db74ce65da602f60779
Plamen Dobrev <46189922+ApPlamen@users.noreply.github.com>
2021-12-19 16:20:24 +0200

    add user rents functionality

--- src/main/java/com/tusofia/LibraryBase/controllers/UserController.java
+++ src/main/java/com/tusofia/LibraryBase/controllers/UserController.java
@@ -1,11 +1,18 @@
 package com.tusofia.LibraryBase.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tusofia.LibraryBase.dtos.inputs.UserInputDTO;
 import com.tusofia.LibraryBase.dtos.inputs.UserUpdateDTO;
+import com.tusofia.LibraryBase.entities.Rent;
+import com.tusofia.LibraryBase.entities.RentActive;
+import com.tusofia.LibraryBase.entities.RentArchive;
 import com.tusofia.LibraryBase.entities.User;
 import com.tusofia.LibraryBase.services.UserService;
 
@@ -13,9 +20,27 @@ import com.tusofia.LibraryBase.services.UserService;
 @RequestMapping("api/user")
 public class UserController extends CRUDController<User, Integer, UserInputDTO, UserUpdateDTO> {
 
+	private UserService service;
+	
 	@Autowired
 	public UserController(UserService service) {
 		super(service);
+		this.service = service;
+	}
+	
+	@GetMapping("get-rents/{entityId}")
+	public List<Rent> getUserRents(@PathVariable int entityId) {
+		return this.service.getUserRents(entityId);
+	}
+	
+	@GetMapping("get-rents-active/{entityId}")
+	public List<RentActive> getUserRentsActive(@PathVariable int entityId) {
+		return this.service.getUserRentsActive(entityId);
+	}
+	
+	@GetMapping("get-rents-archive/{entityId}")
+	public List<RentArchive> getUserRentsArchive(@PathVariable int entityId) {
+		return this.service.getUserRentsArchive(entityId);
 	}
 
 }
--- src/main/java/com/tusofia/LibraryBase/services/UserService.java
+++ src/main/java/com/tusofia/LibraryBase/services/UserService.java
@@ -1,10 +1,17 @@
 package com.tusofia.LibraryBase.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tusofia.LibraryBase.dtos.inputs.UserInputDTO;
 import com.tusofia.LibraryBase.dtos.inputs.UserUpdateDTO;
+import com.tusofia.LibraryBase.entities.Rent;
+import com.tusofia.LibraryBase.entities.RentActive;
+import com.tusofia.LibraryBase.entities.RentArchive;
 import com.tusofia.LibraryBase.entities.User;
 import com.tusofia.LibraryBase.infrastructure.UserRepo;
 
@@ -14,6 +21,30 @@ public class UserService extends CRUDService<User, Integer, UserInputDTO, UserUp
 	@Autowired
 	public UserService(UserRepo repo) {
 		super(repo);
+	}
+	
+	public List<Rent> getUserRents(int userId) {
+		User user = this.repo.findById(userId).get();
+		
+		List<RentActive> rentsActive = user.getRentsActive();
+		List<RentArchive> rentsArchive = user.getRentsArchive();
+		List<Rent> rents = Stream
+				.concat(rentsActive.stream(), rentsArchive.stream())
+				.collect(Collectors.toList());
+		
+		return rents;
+	}
+	
+	public List<RentActive> getUserRentsActive(int userId) {
+		User user = this.repo.findById(userId).get();
+		
+		return user.getRentsActive();
+	}
+	
+	public List<RentArchive> getUserRentsArchive(int userId) {
+		User user = this.repo.findById(userId).get();
+		
+		return user.getRentsArchive();
 	}
 
 }
